39e27211e7286e8fa74731fbf7aa1f88fb9720a4
Amanda Remo <amandaremo@Rickys-MacBook-Pro.local>
2022-05-10 20:17:20 -0400

    updated code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,69 @@
 // TODO: Declare any global variables we need
-
+let heads = 0
+let tails = 0
 
 document.addEventListener('DOMContentLoaded', function () {
-    // This is just a sanity check to make sure your JavaScript script is getting loaded
-    // You can remove it once you see it in your browser console in the developer tools
-    console.log('Hi')
-
     // TODO: Add event listener and handler for flip and clear buttons
+   
+    // Capturing the button click to flip
+    document.getElementById('flip').addEventListener('click', () => {
+        // Determine result by using Math.random which returns a random #
+        // between 0 and 1, therefore it has a 50% chance to be below 0.5
+        // which in our case is heads
+        let flippedHeads = Math.random() < 0.5
+
+        if (flippedHeads) 
+        {
+            document.getElementById('penny-image').setAttribute('src', 'assets/images/penny-heads.jpg')
+            document.getElementById('message').textContent = 'You Flipped Heads!'
+
+            heads++
+        } else 
+        {
+            // some code when tails is flipped
+            document.getElementById('penny-image').setAttribute('src', 'assets/images/penny-tails.jpg')
+            document.getElementById('message').textContent = 'You Flipped Tails!'
+
+            tails++
+        }
+
+        // Update the scoreboard
+
+        // Calculate the total for the statistics
+        let total = heads + tails
+
+        // Variable to save the percentages
+        let percentHeads = 0
+        let percentTails = 0
+
+        // Only calculate stats if we have at least 1 roll
+        if (total > 0)
+        {
+            percentHeads = Math.round((heads / total) * 100)
+            percentTails = Math.round((tails / total) * 100)
+        }
+        
+        // Update the scoreboard table
+        document.getElementById('heads').textContent = heads
+        document.getElementById('heads-percent').textContent = `${percentHeads}%`
+
+        document.getElementById('tails').textContent = tails
+        document.getElementById('tails-percent').textContent = `${percentTails}%`
+    })
+
+    // Capturing the button click to clear
+    document.getElementById('clear').addEventListener('click', () => {
+
+        document.getElementById('message').textContent = `Let's get rolling!`
+        document.getElementById('heads').textContent = heads
+        document.getElementById('heads-percent').textContent = '0%'
+        document.getElementById('tails').textContent = tails
+        document.getElementById('tails-percent').textContent = '0%'       
+
+    })
+
+
+
 
     // Flip Button Click Handler
         // TODO: Determine flip outcome
